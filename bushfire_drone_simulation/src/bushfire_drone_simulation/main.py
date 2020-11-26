c8c5e46bb18025945c3c4e632dd41680b7ac9c6f
u6416832 <u6416832@anu.edu.au>
2020-11-26 22:38:29 +1100

    Displays logging and added sample command arguments

--- bushfire_drone_simulation/src/bushfire_drone_simulation/main.py
+++ bushfire_drone_simulation/src/bushfire_drone_simulation/main.py
@@ -1,6 +1,7 @@
 """Main entry point for bushfire-drone-simulation."""
 
 import logging
+from sys import stderr
 
 import typer
 
@@ -10,13 +11,15 @@ app = typer.Typer()
 
 def main():
     """Entry point for bushfire_drone_simulation."""
+    logging.basicConfig(stream=stderr, level=logging.INFO)
     app()
 
 
 @app.command()
-def func1():
+def func1(x: int = typer.Argument(..., help="Help stuff"), y: int = typer.Option(7, help="Help 2")):
     """Test function 1."""
     _LOG.info("Hello world from func1")
+    print(x, y)
 
 
 @app.command()
