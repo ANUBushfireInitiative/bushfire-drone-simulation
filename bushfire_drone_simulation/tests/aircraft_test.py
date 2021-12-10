afe687866fefa5526136977a4de4a75e6f92d0ba
Elise Palethorpe <ellypaley@gmail.com>
2021-12-10 15:33:53 +1100

    Testing coordinator attributes

--- bushfire_drone_simulation/tests/aircraft_test.py
+++ bushfire_drone_simulation/tests/aircraft_test.py
@@ -1,105 +1,109 @@
 """Aircraft testing."""
 
 from pathlib import Path
+from typing import List
 
 import pytest
 from _pytest.monkeypatch import MonkeyPatch
 
 from bushfire_drone_simulation.aircraft import Status
 from bushfire_drone_simulation.main import run_simulation
+from bushfire_drone_simulation.simulator import Simulator
 
 FILE_LOC = Path(__file__)
 PARAMS_LOC = FILE_LOC.parent / "parameters.json"
 
 
+@pytest.fixture(name="simulations")
+def fixture_simulations(monkeypatch: MonkeyPatch) -> List[Simulator]:
+    """Run a simple simulation and output the results."""
+    monkeypatch.setattr("builtins.input", lambda _: "Y")
+    to_return = []
+    to_return += run_simulation(FILE_LOC.parent / "parameters.json")
+    return to_return
+
+
 @pytest.mark.slow
-def test_times_chronological(monkeypatch: MonkeyPatch) -> None:
+def test_times_chronological(simulations: List[Simulator]) -> None:
     """Are the Aircrafts movements chronological."""
-    monkeypatch.setattr("builtins.input", lambda _: "Y")
-    for simulator in run_simulation(PARAMS_LOC):
+    for simulator in simulations:
         for uav in simulator.uavs:
-            for idx, update in enumerate(uav.past_locations):
-                if idx != 0:
-                    assert (
-                        update.time >= uav.past_locations[idx - 1].time
-                    ), f"The event updates of {uav.get_name()} were not in chronological order"
+            for idx, update in enumerate(uav.past_locations[1:]):
+                assert (
+                    update.time >= uav.past_locations[idx - 1].time
+                ), f"The event updates of {uav.get_name()} were not in chronological order"
         for water_bomber in simulator.water_bombers:
-            for idx, update in enumerate(water_bomber.past_locations):
-                if idx != 0:
-                    assert (
-                        update.time >= water_bomber.past_locations[idx - 1].time
-                    ), f"The event updates of {water_bomber.name} were not in chronological order"
+            for idx, update in enumerate(water_bomber.past_locations[1:]):
+                assert (
+                    update.time >= water_bomber.past_locations[idx - 1].time
+                ), f"The event updates of {water_bomber.name} were not in chronological order"
 
 
 @pytest.mark.slow
-def test_reasonable_fuel_refill(monkeypatch: MonkeyPatch) -> None:
+def test_reasonable_fuel_refill(simulations: List[Simulator]) -> None:
     """Does the Aircraft refill often enough."""
-    monkeypatch.setattr("builtins.input", lambda _: "Y")
-    for simulator in run_simulation(PARAMS_LOC):
+    for simulator in simulations:
         for uav in simulator.uavs:
             time_full = uav.past_locations[0].time
-            for idx, update in enumerate(uav.past_locations):
-                if idx != 0:
-                    if uav.past_locations[idx - 1].status == Status.WAITING_AT_BASE:
-                        time_full = update.time
-                    if update.status == Status.WAITING_AT_BASE:
-                        assert (
-                            update.time - time_full
-                        ) - 1 <= uav.get_range() / uav.flight_speed, (
-                            f"{uav.get_name()} should have run out of fuel"
-                        )
+            for idx, update in enumerate(uav.past_locations[1:]):
+                if uav.past_locations[idx - 1].status == Status.WAITING_AT_BASE:
+                    time_full = update.time
+                if update.status == Status.WAITING_AT_BASE:
+                    assert (
+                        update.time - time_full
+                    ) - 1 <= uav.get_range() / uav.flight_speed, (
+                        f"{uav.get_name()} should have run out of fuel"
+                    )
         for water_bomber in simulator.water_bombers:
             time_full = water_bomber.past_locations[0].time
-            for idx, update in enumerate(water_bomber.past_locations):
-                if idx != 0:
-                    if water_bomber.past_locations[idx - 1].status == Status.WAITING_AT_BASE:
-                        time_full = update.time
-                    if update.status == Status.WAITING_AT_BASE:
-                        has_adequate_fuel = (
-                            update.time - time_full
-                        ) <= water_bomber.get_range() / water_bomber.flight_speed
-                        assert (
-                            has_adequate_fuel
-                        ), f"{water_bomber.get_name()} should have run out of fuel"
-                        time_full = update.time
+            for idx, update in enumerate(water_bomber.past_locations[1:]):
+                if water_bomber.past_locations[idx - 1].status == Status.WAITING_AT_BASE:
+                    time_full = update.time
+                if update.status == Status.WAITING_AT_BASE:
+                    has_adequate_fuel = (
+                        update.time - time_full
+                    ) <= water_bomber.get_range() / water_bomber.flight_speed
+                    assert (
+                        has_adequate_fuel
+                    ), f"{water_bomber.get_name()} should have run out of fuel"
+                    time_full = update.time
 
 
 @pytest.mark.slow
-def test_aircraft_status(monkeypatch: MonkeyPatch) -> None:  # pylint: disable=too-many-branches
+def test_aircraft_status(
+    simulations: List[Simulator],
+) -> None:  # pylint: disable=too-many-branches
     """Does the aircraft status alter reasonably."""
-    monkeypatch.setattr("builtins.input", lambda _: "Y")
-    for simulator in run_simulation(PARAMS_LOC):  # pylint: disable=too-many-nested-blocks
+    for simulator in simulations:  # pylint: disable=too-many-nested-blocks
         for uav in simulator.uavs:
-            for idx, update in enumerate(uav.past_locations):
+            for idx, update in enumerate(uav.past_locations[1:]):
                 assert update.status not in [
                     Status.GOING_TO_WATER,
                     Status.WAITING_AT_WATER,
                 ], f"{uav.get_name()} should not be waiting at or going to water"
-                if idx != 0:
-                    if update.status == Status.WAITING_AT_BASE:
-                        assert (
-                            uav.past_locations[idx - 1].status == Status.GOING_TO_BASE
-                            or Status.WAITING_AT_BASE
-                        ), f"{uav.get_name()} should have previously been going to base"
-                    if update.status == Status.HOVERING:
-                        assert (
-                            uav.past_locations[idx - 1].status != Status.WAITING_AT_BASE
-                        ), f"{uav.get_name()} should have previously been going to strike"
+                if update.status == Status.WAITING_AT_BASE:
+                    assert (
+                        uav.past_locations[idx - 1].status == Status.GOING_TO_BASE
+                        or Status.WAITING_AT_BASE
+                    ), f"{uav.get_name()} should have previously been going to base"
+                if update.status == Status.HOVERING:
+                    assert (
+                        uav.past_locations[idx - 1].status != Status.WAITING_AT_BASE
+                    ), f"{uav.get_name()} should have previously been going to strike"
         for water_bomber in simulator.water_bombers:
-            for idx, update in enumerate(water_bomber.past_locations):
-                if idx != 0:
-                    if update.status == Status.WAITING_AT_BASE:
-                        assert (
-                            water_bomber.past_locations[idx - 1].status == Status.GOING_TO_BASE
-                            or Status.WAITING_AT_BASE
-                        ), f"{water_bomber.name} should have previously been going to base"
-                    if update.status == Status.HOVERING:
-                        assert water_bomber.past_locations[idx - 1].status not in [
-                            Status.WAITING_AT_BASE,
-                            Status.WAITING_AT_WATER,
-                        ], f"{water_bomber.name} should have previously been going to strike"
-                    if update.status == Status.WAITING_AT_WATER:
-                        assert water_bomber.past_locations[idx - 1].status in [
-                            Status.GOING_TO_WATER,
-                            Status.WAITING_AT_WATER,
-                        ], f"{water_bomber.name} should have previously been going to water"
+            for idx, update in enumerate(water_bomber.past_locations[1:]):
+                if update.status == Status.WAITING_AT_BASE:
+                    assert (
+                        water_bomber.past_locations[idx - 1].status == Status.GOING_TO_BASE
+                        or Status.WAITING_AT_BASE
+                    ), f"{water_bomber.name} should have previously been going to base"
+                if update.status == Status.HOVERING:
+                    assert water_bomber.past_locations[idx - 1].status not in [
+                        Status.WAITING_AT_BASE,
+                        Status.WAITING_AT_WATER,
+                    ], f"{water_bomber.name} should have previously been going to strike"
+                if update.status == Status.WAITING_AT_WATER:
+                    assert water_bomber.past_locations[idx - 1].status in [
+                        Status.GOING_TO_WATER,
+                        Status.WAITING_AT_WATER,
+                    ], f"{water_bomber.name} should have previously been going to water"
---
+++ bushfire_drone_simulation/tests/coordinators_test.py
@@ -0,0 +1,141 @@
+"""Coordinators testing.
+
+Compares meaningful attributes of coordinators
+"""
+
+from pathlib import Path
+from statistics import mean
+from typing import List, Optional, Tuple
+
+import pytest
+from _pytest.monkeypatch import MonkeyPatch
+
+from bushfire_drone_simulation.fire_utils import Time
+from bushfire_drone_simulation.main import run_simulation
+from bushfire_drone_simulation.parameters import JSONParameters
+from bushfire_drone_simulation.simulator import Simulator
+
+FILE_LOC = Path(__file__)
+PARAMS_LOC = FILE_LOC.parent / "parameters.json"
+
+
+@pytest.fixture(name="simulations_list")
+def fixture_simulations_list(
+    monkeypatch: MonkeyPatch,
+) -> List[Tuple[List[Simulator], JSONParameters]]:
+    """Run a simple simulation and output the results."""
+    monkeypatch.setattr("builtins.input", lambda _: "Y")
+    to_return: List[Tuple[List[Simulator], JSONParameters]] = []
+    to_return.append(
+        (
+            run_simulation(FILE_LOC.parent / "parameters.json"),
+            JSONParameters(FILE_LOC.parent / "parameters.json"),
+        )
+    )
+    return to_return
+
+
+@pytest.mark.slow
+def test_coordinator_properties(
+    simulations_list: List[Tuple[List[Simulator], JSONParameters]]
+) -> None:
+    """Does the Minimise Mean Time coordinator have the lowest mean time."""
+    for simulators, params in simulations_list:
+        reprocess_max_time_simulation: Optional[Simulator] = None
+        minimise_mean_time_simulation: Optional[Simulator] = None
+        matlab_simulation: Optional[Simulator] = None
+        insertion_simulation: Optional[Simulator] = None
+        for scenario_idx in range(len(params.scenarios)):
+            if (
+                str(params.get_attribute("scenario_name", scenario_idx))
+                == "ReprocessMaxTimeCoordinator"
+            ):
+                reprocess_max_time_simulation = simulators[scenario_idx]
+            if (
+                str(params.get_attribute("scenario_name", scenario_idx))
+                == "MinimiseMeanTimeCoordinator"
+            ):
+                minimise_mean_time_simulation = simulators[scenario_idx]
+            if str(params.get_attribute("scenario_name", scenario_idx)) == "MatlabCoordinator":
+                matlab_simulation = simulators[scenario_idx]
+            if str(params.get_attribute("scenario_name", scenario_idx)) == "InsertionCoordinator":
+                insertion_simulation = simulators[scenario_idx]
+        assert (
+            reprocess_max_time_simulation is not None
+        ), "Not testing Reprocess Max Time coordinator"
+        assert (
+            minimise_mean_time_simulation is not None
+        ), "Not testing Minimise Mean Time coordinator"
+        assert matlab_simulation is not None, "Not testing Matlab Time coordinator"
+        assert insertion_simulation is not None, "Not testing Insertion Time coordinator"
+        assert mean(get_inspection_times(minimise_mean_time_simulation)) >= mean(
+            get_inspection_times(reprocess_max_time_simulation)
+        ), "Minimise mean time returned a higher mean inspection time than Reprocess max time"
+        assert mean(get_inspection_times(minimise_mean_time_simulation)) >= mean(
+            get_inspection_times(insertion_simulation)
+        ), "Minimise mean time returned a higher mean inspection time than Insertion"
+        assert mean(get_inspection_times(minimise_mean_time_simulation)) >= mean(
+            get_inspection_times(matlab_simulation)
+        ), "Minimise mean time returned a higher mean inspection time than Matlab"
+        assert max(get_inspection_times(minimise_mean_time_simulation)) <= max(
+            get_inspection_times(reprocess_max_time_simulation)
+        ), "Reprocess max time returned a higher maximum inspection time than Minimise mean time"
+        assert mean(get_supression_times(minimise_mean_time_simulation)) >= mean(
+            get_supression_times(reprocess_max_time_simulation)
+        ), "Minimise mean time returned a higher mean supression time than Reprocess max time"
+        assert mean(get_supression_times(minimise_mean_time_simulation)) >= mean(
+            get_supression_times(insertion_simulation)
+        ), "Minimise mean time returned a higher mean supression time than Insertion"
+        assert mean(get_supression_times(minimise_mean_time_simulation)) >= mean(
+            get_supression_times(matlab_simulation)
+        ), "Minimise mean time returned a higher mean supression time than Matlab"
+        assert max(get_supression_times(minimise_mean_time_simulation)) <= max(
+            get_supression_times(reprocess_max_time_simulation)
+        ), "Reprocess max time returned a higher max supression time than Minimise mean time"
+
+
+def get_inspection_times(
+    simulation: Simulator,
+) -> List[float]:
+    """Return list of inspection times of lightning strikes.
+
+    Confirms that each strike was inspected.
+
+    Args:
+        simulation (Simulation): simulation that lightning strikes occured in
+
+    Returns:
+        List[float]: list of inspection times
+    """
+    inspection_times: List[float] = []
+    for strike in simulation.lightning_strikes:
+        assert strike.inspected_time is not None, f"Lightning strike {strike} was not inspected"
+        inspection_times.append(
+            Time.from_time(strike.inspected_time - strike.spawn_time).get("hr"),
+        )
+    return inspection_times
+
+
+def get_supression_times(
+    simulation: Simulator,
+) -> List[float]:
+    """Return list of supression times of lightning strikes.
+
+    Confirms that all ignighted strikes were supressed.
+
+    Args:
+        simulation (Simulation): simulation that lightning strikes occured in
+
+    Returns:
+        List[float]: list of supression times
+    """
+    suppression_times: List[float] = []
+    for strike in simulation.lightning_strikes:
+        if strike.ignition:
+            assert (
+                strike.suppressed_time is not None
+            ), f"Lightning strike {strike} ignighted but was not supressed"
+            suppression_times.append(
+                Time.from_time(strike.suppressed_time - strike.spawn_time).get("hr"),
+            )
+    return suppression_times
